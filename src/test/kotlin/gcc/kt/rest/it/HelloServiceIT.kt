0629cb260b499650d680d71d4dc07a507b4e3848
CHARTERS <charters@uk.ibm.com>
2017-10-31 14:04:59 +0000

    First pass at a REST service in Kotlin (on Liberty).

---
+++ src/test/kotlin/gcc/kt/rest/it/HelloServiceIT.kt
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * (c) Copyright IBM Corporation 2017.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package gcc.kt.rest.it
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+import javax.ws.rs.client.Client
+import javax.ws.rs.client.ClientBuilder
+import javax.ws.rs.client.Invocation
+import javax.ws.rs.client.WebTarget
+import javax.ws.rs.core.Response
+
+class HelloServiceIT {
+
+	@Test
+	fun testApplication() {
+        val port = System.getProperty("liberty.test.port")
+        val contextName = System.getProperty("app.context.root")
+        val path = "hello"
+        val person = "Fred"
+        val url = "http://localhost:" + port + "/" + contextName + "/" + path + "/" + person
+
+        val client = ClientBuilder.newClient()
+
+        val target = client.target(url)
+        val response = target.request().get()
+
+        assertEquals("Incorrect response code from " + url, 200, response.getStatus())
+
+        response.close()
+	}
+}
